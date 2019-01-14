03cbc0dc578f6c59e9d955cd373025bfcbde0ef0
Chepell <av.voytenko@gmail.com>
2019-01-14 22:59:32 +0300

    Реализую класс аннотаций, добавляю нееобходимые аннотации и поля.

--- src/Property.java
+++ src/Property.java
@@ -1,8 +1,17 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Artem Voytenko
  * 14.01.2019
  */
 
-
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Property {
+	String propertyName();
+
+	String defaultValue() default "";
 }
