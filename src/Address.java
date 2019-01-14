d10aaef7c7b7d2d4b443c4e58e33386788f89f90
Chepell <av.voytenko@gmail.com>
2019-01-14 20:36:26 +0300

    Базовая реализация класса Address.

--- src/Address.java
+++ src/Address.java
@@ -1,7 +1,31 @@
 /**
  * Artem Voytenko
  * 14.01.2019
+ * <p>
+ * Класс для хранения адреса в виде объекта
  */
-
 public class Address {
+	private String street;
+	private Integer home;
+
+	public Address(String street, Integer home) {
+		this.street = street;
+		this.home = home;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public Integer getHome() {
+		return home;
+	}
+
+	public void setHome(Integer home) {
+		this.home = home;
+	}
 }
